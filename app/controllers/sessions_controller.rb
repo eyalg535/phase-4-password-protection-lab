939b186fa26a38ce287be198e7513f3b87fc8195
eyal gerby <eyalg535@gmail.com>
2023-04-20 14:02:49 -0400

    commit tree 8f89bc4548793cb325d445612e521ccd9aeddcb8

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def create
+    user = User.find_by(username: params[:username])
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      render json: user
+    else
+      render json: { error: "Invalid username or password" }, status: :unauthorized
+    end
+  end
+
+  def destroy
+    session.clear
+    render json: { message: "Logged out" }
+    head :no_content
+  end
+end
